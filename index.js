a5ff7c6d5ad855ac4c07b61a785f2f3b437bee31
Mathis Rome <mathis.rome@icloud.com>
2024-11-05 15:35:18 +0000

    chore: add lint

---
+++ index.js
@@ -0,0 +1,15 @@
+/*eslint semi: ["warning", "always"]*/
+import * as http from 'http'; //ES 6
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n')
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
